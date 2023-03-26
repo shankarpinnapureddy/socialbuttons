a4faf8fa1512d74f0e36c490a69f5e50044b1143
shankar <shankarpnnapureddy@gmail.com>
2023-03-26 08:22:58 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,17 @@
+const Button = (props) => {
+  //  Write your code here.
+};
+
+const element = (
+  //  Write your code here.
+  <div className="container">
+    <div>
+      <h1 className="heading">Social Buttons</h1>
+      <button className="like">Like</button>
+      <button className="comment">Comment</button>
+      <button className="share">Share</button>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
